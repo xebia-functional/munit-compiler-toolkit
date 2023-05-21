af20f81f48143ba43de2614464bd6cc3c5561ecc
Jack C. Viers <jackcviers@gmail.com>
2023-05-21 10:40:50 -0500

    Verifies fixture.compilerTest works

--- munit-compiler-toolkit-testPlugin/src/test/scala/MunitCompilerToolkitTestkitSuite.scala
+++ munit-compiler-toolkit-testPlugin/src/test/scala/MunitCompilerToolkitTestkitSuite.scala
@@ -37,4 +37,53 @@ class MunitCompilerToolkitTestkitSuite extends CompilerSuite:
          |}""".stripMargin
     )
   }
+
+  val sourceFixture = FunFixture[String](
+    setup = _ => {
+      """|def example(s: String): String =
+       |  s
+       |object Thing {
+       |  val x = example("test")
+       |}
+       |""".stripMargin
+    },
+    teardown = _ => ()
+  )
+
+  val expectedTreeFixture = FunFixture[String](
+    setup = _ => {
+      """|package <empty> {
+         |  final lazy module val Thing: Thing = new Thing()
+         |  @SourceFile("compileFromStringscala") final module class Thing() extends Object() { this: Thing.type =>
+         |    private def writeReplace(): AnyRef = new scala.runtime.ModuleSerializationProxy(classOf[Thing.type])
+         |    val x: String = new com.xebia.functional.munitCompilerToolkit.LoggingInterceptor[String](example("test")).apply("example", "test")
+         |  }
+         |  final lazy module val compileFromStringpackage:
+         |    compileFromStringpackage
+         |   = new compileFromStringpackage()
+         |  @SourceFile("compileFromStringscala") final module class
+         |    compileFromStringpackage
+         |  () extends Object() { this: compileFromStringpackage.type =>
+         |    private def writeReplace(): AnyRef =
+         |      new scala.runtime.ModuleSerializationProxy(classOf[compileFromStringpackage.type])
+         |    def example(s: String): String = s
+         |  }
+         |}""".stripMargin
+    },
+    teardown = _ => ()
+  )
+
+  val sourceFixtureAndExpectedTreeFixture =
+    FunFixture.map2(sourceFixture, expectedTreeFixture)
+
+  sourceFixtureAndExpectedTreeFixture.compilerTest(
+    "compilerTest should pick up the TestPlugin from the testArgs"
+  )(Option("pickleQuotes")) { case (source, _) =>
+    source
+  } {
+    case (_, expectedTree) => { case (actualTree, given Context) =>
+      assertEquals(cleanCompilerOutput(actualTree), expectedTree)
+    }
+  }
+
 end MunitCompilerToolkitTestkitSuite
